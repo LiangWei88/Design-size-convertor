b0dc05f6adf2e37bfa2e16e2665db34750927384
wei liang <banemail@gmail.com>
2019-04-19 11:08:48 +0800

    添加静态服务器依赖

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('good');
+var StaticServer = require('static-server');
+var server = new StaticServer({
+    rootPath: '.', // required, the root of the server file tree
+    port: 1337, // required, the port to listen
+});
+server.start(function () {
+    console.log('Server listening to', server.port);
+});
